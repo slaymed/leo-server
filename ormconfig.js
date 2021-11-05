93c68392b155baf3a8c7738e9d238bfa4e2d180b
slaymed <mohabedr34@gmail.com>
2021-11-05 16:57:46 +0100

    update .env

--- ormconfig.js
+++ ormconfig.js
@@ -7,7 +7,6 @@ module.exports = {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     synchronize: DEVELOPMENT_ENV,
     logging: DEVELOPMENT_ENV,
